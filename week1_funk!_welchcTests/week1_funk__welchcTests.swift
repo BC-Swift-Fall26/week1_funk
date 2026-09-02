5f05d1c1e34f12fdc4bfacd7e0883b68fc6b6b45
Cook Welch <cookjwelch@gmail.com>
2026-09-02 14:50:26 -0400

    Initial Commit

---
+++ week1_funk!_welchcTests/week1_funk__welchcTests.swift
@@ -0,0 +1,19 @@
+//
+//  week1_funk__welchcTests.swift
+//  week1_funk!_welchcTests
+//
+//  Created by Cook on 9/2/26.
+//
+
+import Testing
+@testable import week1_funk__welchc
+
+struct week1_funk__welchcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
