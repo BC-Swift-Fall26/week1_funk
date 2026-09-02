eb8ad5263664c4c24f12251026e0a84714f8b861
Cook Welch <cookjwelch@gmail.com>
2026-09-02 14:56:08 -0400

    First commit for week1_funk. Added text, buttons, and images.

--- week1_funk!_welchc/ContentView.swift
+++ week1_funk!_welchc/ContentView.swift
@@ -6,56 +6,40 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State private var imageName = ""
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
+        VStack {
+            Text("Who Brings the Funk?")
+                .font(.largeTitle)
+                .bold()
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
+            HStack {
+                Button("Earth") {
+                    imageName = "globe"
+                }
+                .buttonStyle(.borderedProminent)
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+                Button("Wind") {
+                    imageName = "wind"
+                }
+                .buttonStyle(.borderedProminent)
+
+                Button("Fire") {
+                    imageName = "flame"
+                }
+                .buttonStyle(.borderedProminent)
             }
+
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
